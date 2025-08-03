e177a42a6840b72241b93a0d3c0f3c092b3cb892
NNR0881 <navinkalyanreddy@icloud.com>
2025-08-03 17:23:25 -0400

    Create ResultStateService.cs

---
+++ Services/ResultStateService.cs
@@ -0,0 +1,9 @@
+using SmartDocumentReview.Models;
+
+namespace SmartDocumentReview.Services
+{
+    public class ResultStateService
+    {
+        public List<TagMatch> Matches { get; set; } = new();
+    }
+}
