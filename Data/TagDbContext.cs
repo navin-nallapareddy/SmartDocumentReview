02acf98475b26647f03045fed7703a51918d6517
NNR0881 <navinkalyanreddy@icloud.com>
2025-08-03 15:04:10 -0400

    feat: normalize table and column names

--- Data/TagDbContext.cs
+++ Data/TagDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 using SmartDocumentReview.Models;
 
 namespace SmartDocumentReview.Data
@@ -10,5 +11,26 @@ namespace SmartDocumentReview.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Document> Documents { get; set; }
         public DbSet<TagMatch> TagMatches { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Convert table and column names to lower case to make comparisons case-insensitive
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                var tableName = entity.GetTableName();
+                if (!string.IsNullOrEmpty(tableName))
+                {
+                    entity.SetTableName(tableName.ToLowerInvariant());
+                }
+
+                foreach (var property in entity.GetProperties())
+                {
+                    var columnName = property.GetColumnBaseName();
+                    property.SetColumnName(columnName.ToLowerInvariant());
+                }
+            }
+        }
     }
 }
