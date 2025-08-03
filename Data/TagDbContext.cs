5a20489d77f5e5ab5156aff9e8f3c319501c5786
NNR0881 <navinkalyanreddy@icloud.com>
2025-08-03 12:56:43 -0400

    Add files via upload

--- Data/TagDbContext.cs
+++ Data/TagDbContext.cs
@@ -1,4 +1,3 @@
-
 using Microsoft.EntityFrameworkCore;
 using SmartDocumentReview.Models;
 
@@ -7,6 +6,9 @@ namespace SmartDocumentReview.Data
     public class TagDbContext : DbContext
     {
         public TagDbContext(DbContextOptions<TagDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Document> Documents { get; set; }
         public DbSet<TagMatch> TagMatches { get; set; }
     }
 }
