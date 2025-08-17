9f99789b2772ea320c1085133263f812f3488d3d
NNR0881 <navinkalyanreddy@icloud.com>
2025-08-16 23:08:56 -0400

    Add upload form model, file size limits, and cleanup

--- Models/User.cs
+++ Models/User.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SmartDocumentReview.Models
@@ -5,8 +6,15 @@ namespace SmartDocumentReview.Models
     public class User
     {
         public int Id { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Username { get; set; } = null!;
+
+        [Required]
+        [MaxLength(255)]
+        public string Password { get; set; } = null!;
+
         [Column("CreatedAt")]
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
